f7a237dc6102cf9fafbf2332ade5ea1edbc35142
arsyadsr09 <blacklistdark1987@gmail.com>
2023-07-06 13:28:48 +0700

    Configure firebase

--- lib/utils/injector.dart
+++ lib/utils/injector.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
+import '../firebase_options.dart';
 
 class Injector {
   static Future<void> initializeDependencies() async {
@@ -7,5 +10,9 @@ class Injector {
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
     ]);
+
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
   }
 }
