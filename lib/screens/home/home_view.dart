a128b2c8c6ea471fe95e3112bf60b52b0fa174a3
arsyadsr09 <blacklistdark1987@gmail.com>
2023-07-13 23:11:58 +0700

    Add current city

--- lib/screens/home/home_view.dart
+++ lib/screens/home/home_view.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 import 'package:grid_mobile/helpers/helpers.dart';
+import 'package:grid_mobile/redux/modules/user_state.dart';
+import 'package:grid_mobile/utils/formatter.dart';
 import 'package:grid_mobile/widgets/custom_text.dart';
 
 import '../../localization/app_translations.dart';
+import '../../redux/app_state.dart';
 import './widgets/widgets.dart';
 import './home_view_model.dart';
 
@@ -14,148 +18,157 @@ class HomeView extends HomeViewModel {
         const SystemUiOverlayStyle(statusBarIconBrightness: Brightness.light));
 
     final screenSize = MediaQuery.of(context).size;
-    return ListView(
-      padding: EdgeInsets.zero,
-      shrinkWrap: true,
-      children: [
-        Stack(
-          children: [
-            Container(
-              constraints: BoxConstraints(minHeight: screenSize.height),
-              color: Colors.white,
-            ),
-            Container(
-              height: 230,
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("assets/images/background_home.png"),
-                    alignment: Alignment.bottomCenter,
-                    fit: BoxFit.fitWidth),
-              ),
-            ),
-            Positioned(
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
-                child: SafeArea(
-                  child: Column(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 15),
-                        width: double.maxFinite,
+    return StoreConnector<AppState, UserState>(
+        converter: (store) => store.state.userState,
+        builder: (context, state) {
+          return ListView(
+            padding: EdgeInsets.zero,
+            shrinkWrap: true,
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    constraints: BoxConstraints(minHeight: screenSize.height),
+                    color: Colors.white,
+                  ),
+                  Container(
+                    height: 230,
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image:
+                              AssetImage("assets/images/background_home.png"),
+                          alignment: Alignment.bottomCenter,
+                          fit: BoxFit.fitWidth),
+                    ),
+                  ),
+                  Positioned(
+                      top: 0,
+                      left: 0,
+                      right: 0,
+                      bottom: 0,
+                      child: SafeArea(
                         child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Image.asset(
-                                  "assets/images/grid_logo.png",
-                                  width: 60,
-                                ),
-                                Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    CustomText(
-                                      "Denpasar",
-                                      color: ColorsCustom.white,
-                                      fontSize: 12,
+                            Container(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 15),
+                              width: double.maxFinite,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Image.asset(
+                                        "assets/images/grid_logo.png",
+                                        width: 60,
+                                      ),
+                                      Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          CustomText(
+                                            currentLocation,
+                                            color: ColorsCustom.white,
+                                            fontSize: 12,
+                                          ),
+                                          const SizedBox(width: 5),
+                                          Icon(
+                                            Icons.location_on_outlined,
+                                            color: ColorsCustom.white,
+                                            size: 16,
+                                          )
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                  const SizedBox(height: 7),
+                                  CustomText(
+                                    "Grid Cash",
+                                    color: ColorsCustom.white,
+                                    fontSize: 12,
+                                  ),
+                                  const SizedBox(height: 7),
+                                  Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      CustomText(
+                                        "Rp",
+                                        color: ColorsCustom.white,
+                                        fontSize: 12,
+                                      ),
+                                      const SizedBox(width: 5),
+                                      CustomText(
+                                        Formatters.formatCurrency(state.credit),
+                                        color: ColorsCustom.white,
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 24,
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 20),
+                                  Center(
+                                    child: Container(
+                                      width: double.maxFinite,
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 15),
+                                      decoration: BoxDecoration(
+                                          color: ColorsCustom.primary,
+                                          borderRadius:
+                                              BorderRadius.circular(16)),
+                                      child: Row(children: [
+                                        buttonTopBar(
+                                            icon: Icons.qr_code_scanner_rounded,
+                                            name: AppTranslations.of(context)!
+                                                .text("qr_scan"),
+                                            onTap: () => Navigator.pushNamed(
+                                                context, "/Scanner")),
+                                        buttonTopBar(
+                                            icon: Icons.wallet_outlined,
+                                            name: AppTranslations.of(context)!
+                                                .text("top_up")),
+                                        buttonTopBar(
+                                            icon: Icons.call_made_rounded,
+                                            name: AppTranslations.of(context)!
+                                                .text("send")),
+                                        buttonTopBar(
+                                            icon: Icons.call_received_rounded,
+                                            name: AppTranslations.of(context)!
+                                                .text("request")),
+                                      ]),
                                     ),
-                                    const SizedBox(width: 5),
-                                    Icon(
-                                      Icons.location_on_outlined,
-                                      color: ColorsCustom.white,
-                                      size: 16,
-                                    )
-                                  ],
-                                )
-                              ],
-                            ),
-                            const SizedBox(height: 7),
-                            CustomText(
-                              "Grid Cash",
-                              color: ColorsCustom.white,
-                              fontSize: 12,
-                            ),
-                            const SizedBox(height: 7),
-                            Row(
-                              mainAxisSize: MainAxisSize.min,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                CustomText(
-                                  "Rp",
-                                  color: ColorsCustom.white,
-                                  fontSize: 12,
-                                ),
-                                const SizedBox(width: 5),
-                                CustomText(
-                                  "350.000",
-                                  color: ColorsCustom.white,
-                                  fontWeight: FontWeight.w600,
-                                  fontSize: 24,
-                                ),
-                              ],
-                            ),
-                            const SizedBox(height: 20),
-                            Center(
-                              child: Container(
-                                width: double.maxFinite,
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 15),
-                                decoration: BoxDecoration(
-                                    color: ColorsCustom.primary,
-                                    borderRadius: BorderRadius.circular(16)),
-                                child: Row(children: [
-                                  buttonTopBar(
-                                      icon: Icons.qr_code_scanner_rounded,
-                                      name: AppTranslations.of(context)!
-                                          .text("qr_scan"),
-                                      onTap: () => Navigator.pushNamed(
-                                          context, "/Scanner")),
-                                  buttonTopBar(
-                                      icon: Icons.wallet_outlined,
-                                      name: AppTranslations.of(context)!
-                                          .text("top_up")),
-                                  buttonTopBar(
-                                      icon: Icons.call_made_rounded,
-                                      name: AppTranslations.of(context)!
-                                          .text("send")),
-                                  buttonTopBar(
-                                      icon: Icons.call_received_rounded,
-                                      name: AppTranslations.of(context)!
-                                          .text("request")),
-                                ]),
+                                  ),
+                                  const SizedBox(height: 16),
+                                ],
                               ),
                             ),
-                            const SizedBox(height: 16),
+                            Expanded(
+                              child: Scrollbar(
+                                child: ListView(
+                                    shrinkWrap: true,
+                                    controller: scrollController,
+                                    padding: const EdgeInsets.fromLTRB(
+                                        15, 0, 15, 100),
+                                    children: const [
+                                      StatusCard(),
+                                      SizedBox(height: 16),
+                                      MiniMap(),
+                                      SizedBox(height: 16),
+                                      LatestTransactions(),
+                                    ]),
+                              ),
+                            )
                           ],
                         ),
-                      ),
-                      Expanded(
-                        child: Scrollbar(
-                          child: ListView(
-                              shrinkWrap: true,
-                              controller: scrollController,
-                              padding:
-                                  const EdgeInsets.fromLTRB(15, 0, 15, 100),
-                              children: const [
-                                StatusCard(),
-                                SizedBox(height: 16),
-                                MiniMap(),
-                                SizedBox(height: 16),
-                                LatestTransactions(),
-                              ]),
-                        ),
-                      )
-                    ],
-                  ),
-                )),
-          ],
-        ),
-      ],
-    );
+                      )),
+                ],
+              ),
+            ],
+          );
+        });
   }
 
   Widget buttonTopBar(
--- lib/screens/home/home_view_model.dart
+++ lib/screens/home/home_view_model.dart
@@ -1,9 +1,20 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:grid_mobile/redux/app_state.dart';
+import 'package:redux/redux.dart';
+
 import './home.dart';
 
 abstract class HomeViewModel extends State<Home> {
+  late Store<AppState> store;
   late ScrollController scrollController;
   double scrollPosition = 0.0;
+
+  String currentLocation = "";
 
   void _scrollListener() {
     setState(() {
@@ -11,11 +22,32 @@ abstract class HomeViewModel extends State<Home> {
     });
   }
 
+  Future<void> getCurrentCity() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.best);
+      List<Placemark> placemark =
+          await placemarkFromCoordinates(position.latitude, position.longitude);
+
+      setState(() {
+        currentLocation = (Platform.isIOS
+            ? placemark[0].locality
+            : placemark[0].subAdministrativeArea)!;
+      });
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
   @override
   void initState() {
     scrollController = ScrollController();
     scrollController.addListener(_scrollListener);
+    getCurrentCity();
     super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      store = StoreProvider.of(context);
+    });
   }
 
   @override
