a1d21f819b5794b937bd80c3233cb677e908423f
arsyadsr09 <blacklistdark1987@gmail.com>
2023-07-13 23:12:15 +0700

    Add Charging point page

--- lib/screens/charging_point/charging_point_view.dart
+++ lib/screens/charging_point/charging_point_view.dart
@@ -1,18 +1,232 @@
 import 'package:flutter/material.dart';
-import 'package:grid_mobile/widgets/header_page.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:grid_mobile/helpers/colors_custom.dart';
+import 'package:grid_mobile/redux/modules/payments_state.dart';
+import 'package:grid_mobile/widgets/widgets.dart';
+import '../../localization/app_translations.dart';
+import '../../redux/app_state.dart';
 import './charging_point_view_model.dart';
 
 class ChargingPointView extends ChargingPointViewModel {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          HeaderPage(
-            title: "Charging Point",
-          )
-        ],
-      ),
+      body: StoreConnector<AppState, PaymentsState>(
+          converter: (store) => store.state.paymentsState,
+          builder: (context, state) {
+            return Stack(
+              children: [
+                const HeaderPage(
+                  title: "Charging Points",
+                ),
+                Positioned(
+                  left: 20,
+                  right: 20,
+                  bottom: 0,
+                  top: 90,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CustomText(
+                        AppTranslations.of(context)!.text("location"),
+                        color: ColorsCustom.disabled,
+                        fontSize: 11,
+                        fontWeight: FontWeight.w500,
+                      ),
+                      CustomText(
+                        state.scannedItem?.data?.name,
+                        color: ColorsCustom.black,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      const SizedBox(height: 15),
+                      CustomText(
+                        "CHARGING POINT ID",
+                        color: ColorsCustom.disabled,
+                        fontSize: 11,
+                        fontWeight: FontWeight.w500,
+                      ),
+                      CustomText(
+                        state.scannedItem?.data?.id,
+                        color: ColorsCustom.black,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      const SizedBox(height: 15),
+                      Row(
+                        children: [
+                          Expanded(
+                              child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              CustomText(
+                                AppTranslations.of(context)!
+                                    .text("charging_type"),
+                                color: ColorsCustom.disabled,
+                                fontSize: 11,
+                                fontWeight: FontWeight.w500,
+                              ),
+                              const SizedBox(height: 5),
+                              CustomDropdown(
+                                dataList: chargingTypeData,
+                                name: AppTranslations.of(context)!
+                                    .text("charging_type"),
+                                value: chargingTypeValue,
+                                onChange: setChargingTypeValue,
+                              )
+                            ],
+                          )),
+                          const SizedBox(width: 8),
+                          Expanded(
+                              child: chargingTypeValue == 'Full Charging'
+                                  ? const SizedBox()
+                                  : Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        CustomText(
+                                          AppTranslations.of(context)!
+                                              .text("amount"),
+                                          color: ColorsCustom.disabled,
+                                          fontSize: 11,
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                        const SizedBox(height: 5),
+                                        FormAmount(
+                                          hint: AppTranslations.of(context)!
+                                              .text("first_name"),
+                                          controller: amountController,
+                                          preffix: handlePrefixAmount() == 'IDR'
+                                              ? handlePrefixAmount()
+                                              : "",
+                                          suffix: handlePrefixAmount() == 'KWH'
+                                              ? handlePrefixAmount()
+                                              : "",
+                                          onTap: onAmountTap,
+                                        ),
+                                      ],
+                                    )),
+                        ],
+                      ),
+                      const SizedBox(height: 15),
+                      Row(
+                        children: [
+                          Expanded(
+                              child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              CustomText(
+                                AppTranslations.of(context)!
+                                    .text("parking_ticket"),
+                                color: ColorsCustom.disabled,
+                                fontSize: 11,
+                                fontWeight: FontWeight.w500,
+                              ),
+                              CustomText(
+                                parkingTicketID,
+                                color: ColorsCustom.black,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ],
+                          )),
+                          SizedBox(
+                            height: 30,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 4, horizontal: 10),
+                                  backgroundColor: ColorsCustom.sunflower,
+                                  foregroundColor: ColorsCustom.sunflower,
+                                  elevation: 0,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(4))),
+                              onPressed: onGetImageFromCamera,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    Icons.qr_code_scanner_rounded,
+                                    size: 15,
+                                    color: ColorsCustom.black,
+                                  ),
+                                  const SizedBox(width: 8),
+                                  CustomText(
+                                    AppTranslations.of(context)!
+                                        .text("scan_parking_ticket"),
+                                    color: ColorsCustom.black,
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      const SizedBox(height: 15),
+                      imageSelector != null
+                          ? CustomText(
+                              AppTranslations.of(context)!
+                                  .text("parking_ticket_photo"),
+                              color: ColorsCustom.disabled,
+                              fontSize: 11,
+                              fontWeight: FontWeight.w500,
+                            )
+                          : const SizedBox(),
+                      const SizedBox(height: 4),
+                      SizedBox(
+                        width: double.maxFinite,
+                        height: 200,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: imageSelector != null
+                              ? Image.file(imageSelector!, fit: BoxFit.cover)
+                              : const SizedBox(),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 12, horizontal: 20),
+                    decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(24),
+                          topRight: Radius.circular(24),
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                              offset: const Offset(4, 0),
+                              blurRadius: 12,
+                              spreadRadius: 0,
+                              color: Colors.black.withOpacity(0.1))
+                        ],
+                        color: ColorsCustom.white),
+                    child: CustomButton(
+                      onPressed: onContinue,
+                      text: AppTranslations.of(context)!.text("continue"),
+                      textColor: ColorsCustom.white,
+                      bgColor: ColorsCustom.pomegrande,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 12,
+                      borderRadius: BorderRadius.circular(8),
+                      padding: const EdgeInsets.symmetric(vertical: 8),
+                      margin: EdgeInsets.zero,
+                    ),
+                  ),
+                )
+              ],
+            );
+          }),
     );
   }
 }
--- lib/screens/charging_point/charging_point_view_model.dart
+++ lib/screens/charging_point/charging_point_view_model.dart
@@ -1,6 +1,87 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:grid_mobile/redux/actions/payments_action.dart';
+import 'package:grid_mobile/redux/app_state.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:redux/redux.dart';
+import '../../constants/enum/payment_enum.dart';
+import '../../models/models.dart';
+import '../payment/payment.dart';
 import './charging_point.dart';
 
 abstract class ChargingPointViewModel extends State<ChargingPoint> {
-  // Add your state and logic here
+  late Store<AppState> store;
+  final ImagePicker picker = ImagePicker();
+
+  TextEditingController amountController = TextEditingController(text: '0');
+
+  List chargingTypeData = ["Full Charging", "IDR Amount", "KWH Amount"];
+
+  String chargingTypeValue = "IDR Amount";
+  String parkingTicketID = "0173564ACCB";
+
+  File? imageSelector;
+
+  void setChargingTypeValue(String value) {
+    setState(() {
+      chargingTypeValue = value;
+    });
+  }
+
+  void onAmountTap() {
+    setState(() {
+      amountController.clear();
+    });
+  }
+
+  String handlePrefixAmount() {
+    if (chargingTypeValue == 'IDR Amount') {
+      return "IDR";
+    } else {
+      return "KWH";
+    }
+  }
+
+  Future<void> onGetImageFromCamera() async {
+    final XFile? image = await picker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      imageSelector = File(image!.path);
+    });
+  }
+
+  Future<void> onContinue() async {
+    int amount = int.parse(amountController.text.replaceAll(',', ''));
+    int tax = (amount * 0.1).round();
+    PaymentModel payment = PaymentModel(
+      amount: amount,
+      chargingType: chargingTypeValue,
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      tax: tax,
+      total: amount + tax,
+      parkingTicket: parkingTicketID,
+    );
+
+    ScannedItem data =
+        store.state.paymentsState.scannedItem!.copyWith(payment: payment);
+    await store.dispatch(SetScannedItem(scannedItem: data));
+
+    // ignore: use_build_context_synchronously
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (_) => const Payment(
+                  type: PaymentTypeEnum.charging,
+                )));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      store = StoreProvider.of(context);
+    });
+  }
 }
